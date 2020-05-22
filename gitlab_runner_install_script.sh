d11d2529c2b1fcf812ff0b78ca9ca56dabd28134
trg78 <35064447+trg78@users.noreply.github.com>
2020-05-22 13:55:32 +0300

    Update gitlab_runner_install_script.sh

--- gitlab_runner_install_script.sh
+++ gitlab_runner_install_script.sh
@@ -7,7 +7,6 @@ touch /etc/apt/sources.list.d/runner_gitlab-runner.list
 echo deb https://packages.gitlab.com/runner/gitlab-runner/ubuntu/ focal main
 deb-src https://packages.gitlab.com/runner/gitlab-runner/ubuntu/ focal main > /etc/apt/sources.list.d/runner_gitlab-runner.list
 apt update
-curl -s https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
 export GITLAB_RUNNER_DISABLE_SKEL=true;
 apt install -y gitlab-runner
 gitlab-runner register
